9b29f10fb133598d133e3883527200e65df2c122
denilsoncoutinho <denilsoncoutinho9@gmail.com>
2025-03-04 23:10:15 -0300

    adicionando novos template

--- src/app/components/admin/socialProfile.tsx
+++ src/app/components/admin/socialProfile.tsx
@@ -8,19 +8,51 @@ import { useState } from "react";
 import { Session } from "next-auth";
 import CreateLink from "../../../../actions/createLink";
 import { usePreviews } from "../../../../context/triggerPreview";
-
-
+import 'react-image-crop/dist/ReactCrop.css'
+import ReactCrop, { type Crop } from 'react-image-crop'
 
 export default function SocialProfile({ session }: { session: Session | null }) {
     function textSlice(text: string) {
         return text.substring(0, 40) + "..."
     }
     const { preview } = usePreviews()
+    // const [crop, setCrop] = useState<Crop>({
+    //     unit: '%', // Can be 'px' or '%'
+    //     x: 25,
+    //     y: 25,
+    //     width: 50,
+    //     height: 50
+    //   })
+
+    // async function handleUpload(): Promise<responseUpload> {
+    //     const uploads = imageCouple.map((image: any) => {
+    //         const storageRef = ref(storage, `user/${idUser}/images/${image?.name}`);
+    //         return uploadBytes(storageRef, image) // Faz o upload de cada foto
+    //             .then((snapshot) => {
+
+    //                 return getDownloadURL(snapshot.ref); // Obtém a URL de download
+    //             })
+    //             .catch((error) => {
+    //                 console.error(`Erro ao enviar foto ${image}:`, error);
+    //             });
+    //     })
+
+    //     try {
+    //         const urls = await Promise.all(uploads);
+    //         return { imgUpload: urls }
+    //     } catch (error) {
+    //         console.error("Erro ao enviar as fotos:", error);
+    //         return { errorImg: "Algo deu errado!" }
+    //     }
+    // }
     return (
         <div className={`${preview ? "hidden" : "flex"} flex-col w-full  py-2 px-2 bg rounded-md border-slate-700`}>
             <div className="flex items-center gap-2">
                 <div className="flex flex-col items-center ">
                     <div className="relative border shadow-md w-16 h-16 rounded-full overflow-hidden bg-cover text-transparent bg-no-repeat bg-center p-3" >
+                        {/* <ReactCrop crop={crop} onChange={c => setCrop(c)}>
+                            <Image src={imageProfile} alt="imageProfile" />
+                        </ReactCrop> */}
                         <Image
                             src={imageProfile}
                             alt="Profile"
